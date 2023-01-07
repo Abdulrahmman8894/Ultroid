b0d9dd7a6141b5d4fc66570aba64db9cec0bdb96
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-07 21:50:17 +0300

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -25,7 +25,7 @@ from . import get_string, requests, ultroid_cmd
 
 
 @ultroid_cmd(
-    pattern="yt(a|v|sa|sv) ?(.*)",
+    pattern="يوتيوب ?(.*)",
 )
 async def download_from_youtube_(event):
     ytd = {
