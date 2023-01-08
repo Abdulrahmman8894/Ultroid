ca6e706bde34676b9b6d439415b821afe759d01a
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-08 13:50:00 +0300

    Update __init__.py

--- pyUltroid/dB/__init__.py
+++ pyUltroid/dB/__init__.py
@@ -10,24 +10,15 @@ if not run_as_module:
 from .. import *
 
 DEVLIST = [
-    719195224,  # @xditya
-    1322549723,  # @danish_00
-    1903729401,  # @its_buddhhu
-    1303895686,  # @Sipak_OP
-    611816596,  # @Arnab431
-    1318486004,  # @sppidy
-    803243487,  # @hellboi_atul
+    5502537272,  # @ZQ_LO
 ]
 
 ULTROID_IMAGES = [
     f"https://graph.org/file/{_}.jpg"
     for _ in [
-        "ec250c66268b62ee4ade6",
-        "3c25230ae30d246194eba",
-        "b01715a61b9e876c0d45d",
-        "4ceaf720a96a24527ecff",
-        "a96223b574f29f3f0d184",
-        "6e081d339a01cc6190393",
+        "b3e0cb631d36672092852",
+        "56b2ec14ede1b877cbdf7",
+        "a6fb8a39f0aebe1d98570",
     ]
 ]
 
